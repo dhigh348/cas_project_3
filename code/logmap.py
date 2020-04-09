ee6534f402ea7603a21c75a24fc7cf858f5f0bef
Danan Q High <dqhigh@cs.unm.edu>
2020-04-09 12:20:37 -0600

    first commit

---
+++ code/logmap.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import math 
+
+class LogMap:
+
+	def __init__(self, r, num_its):
+		self.r = r
+		self.num_its = num_its
+
+	def log_map_simulation(self):
+
+
+	def make_data(self, file_name):
